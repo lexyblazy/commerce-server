952641d8b4e9ec60d4ddc044e36270ea9255978d
lexyblazy <lexyblazy@gmail.com>
2020-10-05 14:00:27 +0100

    fix travis and dev dockerfile

--- src/app.ts
+++ src/app.ts
@@ -2,17 +2,20 @@ import express from "express";
 import { loadServices } from "./loadServices";
 import { loadRouters } from "./loadRouters";
 
-const app = express();
-const PORT = 5000;
+const main = async () => {
+  const app = express();
+  const PORT = 5000;
 
-const loadServicesPromise = loadServices();
-const loadServicesAndRoutersPromise = loadRouters(loadServicesPromise, app);
+  await loadServices();
+  await loadRouters(app);
 
-app.use(async (_req, _res, next) => {
-  await loadServicesAndRoutersPromise;
-  next();
-});
+  app.listen(PORT);
+};
 
-app.listen(PORT, () => {
-  console.log("Server is up and running");
-});
+main()
+  .then(() => {
+    console.log("Application Loaded");
+  })
+  .catch((e) => {
+    console.log(console.log(e));
+  });
--- src/loadRouters.ts
+++ src/loadRouters.ts
@@ -1,12 +1,7 @@
 import * as express from "express";
 import HttpStatus from "http-status-codes";
 
-export const loadRouters = async (
-  loadServicesPromise: LoadServicesPromise,
-  app: express.Application
-) => {
-  await loadServicesPromise;
-
+export const loadRouters = async (app: express.Application) => {
   app.get("/", (_req, res) => {
     res.sendStatus(HttpStatus.OK);
   });
