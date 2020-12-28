dcf521fa8f6dbda330d6c99ce03a56fe4180b467
lexyblazy <lexyblazy@gmail.com>
2020-12-28 22:37:02 +0100

    add cors

--- src/loadRouters.ts
+++ src/loadRouters.ts
@@ -1,10 +1,13 @@
 import * as express from "express";
+import cors from "cors";
 import HttpStatus from "http-status-codes";
 
 import * as merchants from "./merchants";
 import * as sessions from "./sessions";
 
 export const loadRouters = async (app: express.Application) => {
+  app.use(cors({ origin: ["http://localhost:3000"], credentials: true }));
+
   app.use("/merchants", merchants.router.create());
 
   app.use("/sessions", sessions.router.create());
