f02d318be75fa503cb52f25674328ecf7ab5842d
lexyblazy <lexyblazy@gmail.com>
2021-04-25 11:22:45 +0100

    imporove types and tweaks

--- src/kms/types.d.ts
+++ src/kms/types.d.ts
@@ -1,1 +1,7 @@
-type Environment = "development" | "staging";
+type Environment = keyof Settings;
+
+type SettingsUnion =
+  | (Settings["development"]["plain"] & Settings["development"]["encrypted"])
+  | (Settings["staging"]["plain"] & Settings["staging"]["encrypted"]);
+
+type KMSSettingKey = keyof SettingsUnion;
