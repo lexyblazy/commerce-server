fbe69421ddb3e3f2f45ccf133c7127c77bf98477
lexyblazy <lexyblazy@gmail.com>
2021-04-26 22:36:45 +0100

    loadservices and routers in only development mode

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import bodyParser from "body-parser";
 import express from "express";
+import HttpStatus from "http-status-codes";
+
+import * as utils from "./utils";
+
 import { loadRouters } from "./loadRouters";
 import { loadServices } from "./loadServices";
 
@@ -11,8 +15,16 @@ const main = async () => {
 
   const PORT = process.env.PORT ?? 5000;
 
-  await loadServices();
-  await loadRouters(app);
+  // load database connections and kms only in development
+  // AWS charges are killing me.
+  if (utils.environment.isDevelopment()) {
+    await loadServices();
+    await loadRouters(app);
+  }
+
+  app.get("/", (_req, res) => {
+    res.sendStatus(HttpStatus.OK);
+  });
 
   app.listen(PORT);
 };
--- src/loadRouters.ts
+++ src/loadRouters.ts
@@ -1,6 +1,5 @@
 import * as express from "express";
 import cors from "cors";
-import HttpStatus from "http-status-codes";
 
 import * as kms from "./kms";
 import * as merchants from "./merchants";
@@ -17,8 +16,4 @@ export const loadRouters = async (app: express.Application) => {
   app.use("/merchants", merchants.router.create());
 
   app.use("/sessions", sessions.router.create());
-
-  app.get("/", (_req, res) => {
-    res.sendStatus(HttpStatus.OK);
-  });
 };
